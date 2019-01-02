c07a2015444cea7bccc9a55a4305ee8c1cb6f787
JacobHicks <jacobphicks@gmail.com>
2019-01-02 00:49:20 -0600

    Redid project structure cause im dumb

---
+++ src/main/java/com/seji/kidcuiside/KidCuisIdeApplication.java
@@ -0,0 +1,14 @@
+package com.seji.kidcuiside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidCuisIdeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KidCuisIdeApplication.class, args);
+    }
+
+}
+

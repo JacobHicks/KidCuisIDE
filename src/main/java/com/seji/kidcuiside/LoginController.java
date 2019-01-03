d301a2427a26f34c5951540040c09098ee6e335a
JacobHicks <jacobphicks@gmail.com>
2019-01-02 19:09:13 -0600

    Working with forms pt 2
    Signed-off-by: JacobHicks <jacobphicks@gmail.com>

--- src/main/java/com/seji/kidcuiside/LoginController.java
+++ src/main/java/com/seji/kidcuiside/LoginController.java
@@ -6,23 +6,23 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class LoginController {
-
     @GetMapping("/login")
     public String login(Model model) {
         model.addAttribute("login", new LoginBean());
         return "login";
-        //TODO send data to server
     }
+
     @PostMapping("/login")
     public String loginRequest(@ModelAttribute LoginBean login) {
         String user = login.getUsername();
         String password = login.getPassword();
-        if(user == null || user.length() < 5 || user.contains("\\W") || password == null || password.length() < 6) {
-            return "login";
+        if(user == null || user.length() < 5 || user.length() > 64 || user.contains("\\W") || (user+password).contains("\\s") || password == null || password.length() < 6 || password.length() > 128) {
+            return "redirect:/login";
         }
         else {
-            
+            String hash = Cryptor.hash("Kid" + user + "Cuis" + password + "IDE");
+            //TODO check hash against DB
         }
-        return "result";
+        return "redirect:/CodeWritingWindow";
     }
 }

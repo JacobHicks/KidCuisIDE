8cfed89aa195d4bf2b88de082fe5c46259727fa5
Saffat Ahmed <koolsaffat@gmail.com>
2019-02-26 11:02:56 -0600

    gets resources so it looks nice when running the program

--- src/main/java/com/seji/kidcuiside/CodeWindowController.java
+++ src/main/java/com/seji/kidcuiside/CodeWindowController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @Controller
 public class CodeWindowController {
@@ -11,5 +12,10 @@ public class CodeWindowController {
     public String code(Model model, @CookieValue(value = "id", defaultValue = "") String id) {
         if(id.equals("")) return "redirect:/login";
         return "CodeWritingWindow";
+    }
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/easyui/**").addResourceLocations("classpath:/easyui/");
+        registry.addResourceHandler("/codemirror/**").addResourceLocations("classpath:/codemirror/");
+        registry.addResourceHandler("/easyui/**").addResourceLocations("classpath:/easyui/");
     }
 }

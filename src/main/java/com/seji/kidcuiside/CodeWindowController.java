61999862a154c922c8d30abd29f3348fa262486f
JacobHicks <$oYbeans1218>
2019-02-26 15:27:13 -0600

    Changed port to allow running on school macs

--- src/main/java/com/seji/kidcuiside/CodeWindowController.java
+++ src/main/java/com/seji/kidcuiside/CodeWindowController.java
@@ -8,11 +8,13 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 
 @Controller
 public class CodeWindowController {
+
     @GetMapping("/code")
     public String code(Model model, @CookieValue(value = "id", defaultValue = "") String id) {
         if(id.equals("")) return "redirect:/login";
         return "CodeWritingWindow";
     }
+
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/easyui/**").addResourceLocations("classpath:/easyui/");
         registry.addResourceHandler("/codemirror/**").addResourceLocations("classpath:/codemirror/");

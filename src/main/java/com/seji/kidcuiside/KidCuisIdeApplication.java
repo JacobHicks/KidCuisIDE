7d9818de4d72250ec15614e0a681698989e9b4ef
JacobHicks <$oYbeans1218>
2019-02-26 22:31:46 -0600

    redid log files v2

--- src/main/java/com/seji/kidcuiside/KidCuisIdeApplication.java
+++ src/main/java/com/seji/kidcuiside/KidCuisIdeApplication.java
@@ -12,13 +12,10 @@ public class KidCuisIdeApplication {
     public static void main(String[] args) {
         try {
             File outputlog = new File("logs/" + Calendar.getInstance().getTime() + "-out.log");
-            File errlog = new File("logs/" + Calendar.getInstance().getTime() + "-err.log");
             outputlog.createNewFile();
-            errlog.createNewFile();
             PrintStream out = new PrintStream(outputlog);
-            PrintStream err = new PrintStream(errlog);
             System.setOut(out);
-            System.setErr(err);
+            System.setErr(out);
         } catch (IOException e) {
             System.err.println("Unable to start logging, printing to terminal instead");
             e.printStackTrace();

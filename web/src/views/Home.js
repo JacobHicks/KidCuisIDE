5a0dfcd29f3cb7d81c0ca209644a1886769f2c56
Ismail Hozain <ismailhozain@gmail.com>
2019-06-26 13:00:20 -0500

    refined homepage a little more. closer to done.

--- web/src/views/Home.js
+++ web/src/views/Home.js
@@ -6,8 +6,9 @@ const React = require('react');
 export default class Home extends React.Component {
     render() {
         return (
-
+            <html>
             <body>
+            <div className="wrapper">
             <div className="navDiv">
                 <Link to="/login">
                     <button className="loginButtons">Login</button>
@@ -26,15 +27,25 @@ export default class Home extends React.Component {
                 <div id="section2" className="section">
                     <h1 className="itemHeading">School Oriented IDE</h1>
                     <h2 className="helperTitle">With teacher accounts</h2>
-                    <p className="talk">WebIDE comes with teacher and student logins that allow teachers to monitor and work with students individually.</p>
+                    <img  className="helperImage" src="https://previews.123rf.com/images/stevanovicigor/stevanovicigor1508/stevanovicigor150800150/43924569-tv-damage-bad-sync-tv-channel-rgb-lcd-television-screen-with-static-noise-from-poor-broadcast-signal.jpg"></img>
+
+                    <p className="talk">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." WebIDE comes with teacher and student logins that allow teachers to monitor and work with students individually.</p>
+                    <div className="buttonDiv">
+                        <a href="#section3"><button className="launch" >&#709;</button></a>
+                    </div>
                 </div>
                 <div id="section3" className="section">
-                    <h1 className="itemHeading">Ready to work whenever and wherever you are.</h1>
+                    <h1 className="itemHeading">Ready to work wherever you are.</h1>
                     <h2 className="helperTitle">A full featured cloud-based IDE</h2>
-                    <p className="talk">WebIDE works in the cloud, in any browser, and is always ready for anything from homework assignments to multi-year projects.</p>
+                    <p className="talk">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum." WebIDE works in the cloud, in any browser, and is always ready for anything from homework assignments to multi-year projects.</p>
+                    <div className="buttonDiv">
+                        <a href="#section4"><button className="launch" >&#709;</button></a>
+                    </div>
                 </div>
             </div>
+            </div>
             </body>
+            </html>
         )
     }
 }

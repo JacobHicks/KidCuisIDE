d11780b20c99f88aa372697ea91830def2e41ab5
Ismail Hozain <ismailhozain@gmail.com>
2019-06-26 11:09:29 -0500

    refined homepage a little more. Not anywhere near done.

--- web/src/views/Home.js
+++ web/src/views/Home.js
@@ -23,10 +23,15 @@ export default class Home extends React.Component {
                 <div className="buttonDiv">
                     <a href="#section2"><button className="launch" >&#709;</button></a>
                 </div>
-                <div id="section2">
+                <div id="section2" className="section">
                     <h1 className="itemHeading">School Oriented IDE</h1>
-                    <h2 className="helperTitle">With Teacher logins</h2>
+                    <h2 className="helperTitle">With teacher accounts</h2>
                     <p className="talk">WebIDE comes with teacher and student logins that allow teachers to monitor and work with students individually.</p>
+                </div>
+                <div id="section3" className="section">
+                    <h1 className="itemHeading">Ready to work whenever and wherever you are.</h1>
+                    <h2 className="helperTitle">A full featured cloud-based IDE</h2>
+                    <p className="talk">WebIDE works in the cloud, in any browser, and is always ready for anything from homework assignments to multi-year projects.</p>
                 </div>
             </div>
             </body>

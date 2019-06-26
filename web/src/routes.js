edadd7358c0aa464427c6e80dc94514454a899df
Saffat Ahmed <koolsaffat@gmail.com>
2019-06-26 13:56:44 -0500

    added basic teacher dashboard

--- web/src/routes.js
+++ web/src/routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route, BrowserRouter as Router } from 'react-router-dom'
 import Home from "./views/Home";
 import Code from "./views/Code";
+import Teacher from "./views/Teacher"
 
 export default (
     <Router>
         <Route exact path='/' component={Home} />
         <Route path='/code' component={Code}/>
+        <Route path='/teacher' component={Teacher}/>
     </Router>
 );
